88ff7039695c18a3780e60eb1f4fdfb82d3e6f8b
Gabriel7ven <gabriel.oficial@yahoo.com>
2023-02-06 11:26:02 -0300

    testes

---
+++ main.js
@@ -0,0 +1,77 @@
+//EXERCÍCIO 1 - Imprima todos os números de 1 a 10
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//EXERCICIO 2 - Calcule a soma de todos os números de 1 a 100
+// let soma = 0;
+// for(let i = 1; i <= 100; i++) {    
+//     soma += i;
+//     console.log(soma)
+// }
+
+//EXERCICIO 3 - Imprima todos os números pares de 1 a 20 
+// for(let i = 2; i <= 20; i += 2){
+//     console.log(i)
+// }
+
+//EXERCICIO 4 - Imprima a tabuada do número 7
+// let total = 0;
+// let num = 7;
+// for(let i = 1; i <= 10; i++) {
+//     total = num * i;
+//     console.log(num + " x " + i + " = " + total);
+// }
+
+//EXERCICIO 5 - Cacule a soma dos primeiros 10 números ímpares.
+// let soma = 0;
+// for(let i = 1; i <=20; i += 2) {
+//     soma += i;
+//     console.log(soma);
+// }
+
+//EXERCICIO 6 - Encontre o maior número em um array
+// let arr = [2, 3, 6, 1, 300, 2, 1]
+// let max = arr[2];
+// for(let i = 1; i < arr.length; i++) {
+//    if(arr[i] > max) {
+//     max = arr[i];
+//    }
+       
+// }
+//  console.log(max)
+
+// EXERCICIO 7 - Enconre o menor número em um array
+// let arr = [15,8,7,6,65,23,9];
+// let min = arr[0];
+// for(let i = 1; i < arr.length; i++) {
+//         if(arr[i] < min) {
+//             min = arr[i];
+//         }    
+//     }
+// console.log(min)
+
+//EXERCICIO 8 - Imprima todos os elementos de um array
+// let arr = [15,8,7,6,65,23,9];
+// for(let i= 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+//EXERCICIO 9 - Verifique se um numero especifico está presente em um array
+// let arr = [15,8,7,6,65,23,9];
+// let num = 7;
+// for(let i= 0; i < arr.length; i++){
+//      if(arr[i] === num){
+//         console.log('Este número existe dentro do array! É o elemento de índice ' + i + "!")
+//         break; //pra que a verificação termine qundo encontrado o número
+//      } 
+//  }
+
+//EXERCICIO 10 - Calcule a média de todos os elementos em um array
+// let arr = [15,8,7,6,65,23,9];
+// let soma = 0;
+// for(let i = 0; i < arr.length; i++){
+//     soma = soma + arr[i];
+// }
+// console.log(soma/arr.length)
+
